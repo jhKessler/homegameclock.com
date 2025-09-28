bd30c4a54ce95c9bd3aa9c2c1374cf80192d4839
Johnny Kessler <68814636+jhKessler@users.noreply.github.com>
2025-09-28 13:04:53 +0200

    improve

--- src/app/timer/timer-page-wrapper.tsx
+++ src/app/timer/timer-page-wrapper.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useTimerStore } from "~/stores/timer/timer-store-provider"
 import { useInterval } from "usehooks-ts"
-import { useMemo, useState } from "react"
+import { useMemo, useRef, useState } from "react"
 import { useConfigStore } from "~/stores/config/config-store-provider"
 import type { CurrentLevelInfo } from "~/stores/config/config-store"
 import PokerTimerDisplay from "./poker-timer-display"
@@ -14,20 +14,24 @@ export default function TimerPageWrapper() {
   const { startTime, reset, pausedTime, getTimeElapsed } = useTimerStore((state) => state)
   const [secondsSinceStart, setSecondsSinceStart] = useState<number | null>(null)
   const [currentLevelInfo, setCurrentLevelInfo] = useState<CurrentLevelInfo | null>(null)
-  const router = useRouter();
-  const totalPricePool = useMemo(() => {
-    if (!playerOptions.players || !playerOptions.buyIn) return null;
-    return (playerOptions.players) * (playerOptions.buyIn)
-  }, [playerOptions])
-  
 
-  useInterval(() => {
-    const timeElapsed = getTimeElapsed();
-    setSecondsSinceStart(Math.floor(timeElapsed / 1000))
-    setCurrentLevelInfo(getCurrentLevel(timeElapsed))
-  },
+  const router = useRouter()
+
+  const totalPricePool = useMemo(() => {
+    if (!playerOptions.players || !playerOptions.buyIn) return null
+    return playerOptions.players * playerOptions.buyIn
+  }, [playerOptions])
+
+  useInterval(
+    () => {
+      const timeElapsed = getTimeElapsed()
+      const currentLevel = getCurrentLevel(timeElapsed)
+
+      setSecondsSinceStart(Math.floor(timeElapsed / 1000))
+      setCurrentLevelInfo(currentLevel)
+    },
     !secondsSinceStart || (startTime && !pausedTime) ? MS_CHECK_INTERVAL : null
-)
+  )
 
   return (
     <PokerTimerDisplay
@@ -36,7 +40,7 @@ export default function TimerPageWrapper() {
       totalPricePool={totalPricePool}
       onReset={() => {
         reset()
-        router.push('/')
+        router.push("/")
       }}
       isPaused={!!pausedTime}
     />
